0af8d7cd596d61a8837f4a3128ee5d51531f6326
DESKTOP-QP6E4JG\Valtas <Kostoev-23@yandex.ru>
2022-12-06 09:45:30 +0300

    string array generator

---
+++ finalTask/Program.cs
@@ -0,0 +1,25 @@
+﻿//Из массива строк формирует массив с длинной строк 3 и миньше.
+
+string GenerateString()
+{
+    Random rnd = new Random();
+    int number = rnd.Next();
+    int rndString = number;
+    return rndString.ToString();
+}
+void FillStringArray(string[] arr)
+{
+    int length = arr.Length;
+    for (int i = 0; i < length; i++)
+    {
+        arr[i] = GenerateString();
+    }
+}
+void PrintArray(string[] arr)
+{
+    int length = arr.Length;
+    for (int i = 0; i < length; i++)
+    {
+        Console.WriteLine(arr[i]);
+    }
+}
