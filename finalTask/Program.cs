834eecc35e1d0c7128ae0f807c60c72555be989a
DESKTOP-QP6E4JG\Valtas <Kostoev-23@yandex.ru>
2022-12-06 10:09:00 +0300

    string filter done

--- finalTask/Program.cs
+++ finalTask/Program.cs
@@ -1,9 +1,9 @@
-﻿//Из массива строк формирует массив с длинной строк 3 и миньше.
+﻿//Из массива строк формирует массив с длинной строк 3 и меньше.
 
 string GenerateString()
 {
     Random rnd = new Random();
-    int number = rnd.Next();
+    int number = rnd.Next(10, 2000);
     int rndString = number;
     return rndString.ToString();
 }
@@ -20,6 +20,20 @@ void PrintArray(string[] arr)
     int length = arr.Length;
     for (int i = 0; i < length; i++)
     {
-        Console.WriteLine(arr[i]);
+        Console.Write($"{arr[i]} ");
+    }
+}
+
+string[] array = new string[10];
+
+FillStringArray(array);
+PrintArray(array);
+Console.WriteLine();
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i].Length <= 3)
+    {
+        Console.Write($"{array[i]} ");
     }
 }
